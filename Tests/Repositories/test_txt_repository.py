191131bd546b59b0d7416d79a2b51396b9d56a0f
Otto <otto@masterbranch.io>
2020-04-03 04:44:04 +0100

    Update test_txt_repository.py

--- Tests/Repositories/test_txt_repository.py
+++ Tests/Repositories/test_txt_repository.py
@@ -4,19 +4,19 @@ from Repositories import TxtRepository
 
 class TxtRepositoryTests(unittest.TestCase):
 
-    def test_get_headers_returns_headers(self):
+    def test_read_file_returns_empty_result_for_empty_file(self):
         txt_file = 'Tests/TestEnvironment/empty.txt'
         repo = TxtRepository()
         result = repo.read_file(txt_file)
         self.assertEqual(0, len(result))
 
-    def test_get_metadata_raisesStopIteration_on_missing_description_row(self):
+    def test_read_file_returns_single_line_result_for_single_line_file(self):
         txt_file = 'Tests/TestEnvironment/single_line.txt'
         repo = TxtRepository()
         result = repo.read_file(txt_file)
         self.assertEqual(1, len(result))
 
-    def test_get_metadata_returns_description_row(self):
+    def test_read_file_returns_every_line_for_multiple_lines_file(self):
         txt_file = 'Tests/TestEnvironment/multi_line.txt'
         repo = TxtRepository()
         result = repo.read_file(txt_file)
